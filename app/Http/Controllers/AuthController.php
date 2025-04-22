5e070febb495a1de8f988e783474c721243ff59b
Кухарук Александр <okukharuk@expertus.com.ua>
2025-04-23 00:37:47 +0300

    added auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Дані не відповідають нашим записам.'],
+            ]);
+        }
+
+        // Створення токена з можливими правами доступу
+        return ['token' => $user->createToken($request->device_name)->plainTextToken];
+    }
+
+    public function logout(Request $request)
+    {
+        // Видалення поточного токена
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out']);
+    }
+}
