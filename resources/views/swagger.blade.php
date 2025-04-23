07a20a8d56e90ef8e9993751d4073a6daa23e2f0
Кухарук Александр <okukharuk@expertus.com.ua>
2025-04-23 22:33:27 +0300

    added swagger

---
+++ resources/views/swagger.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Swagger UI</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+  <script>
+    window.onload = () => {
+      SwaggerUIBundle({
+        url: "/docs/swagger.yaml", // <- тут шлях до твого JSON
+        dom_id: "#swagger-ui",
+        presets: [
+          SwaggerUIBundle.presets.apis
+        ],
+        layout: "BaseLayout"
+      });
+    };
+  </script>
+</body>
+</html>
