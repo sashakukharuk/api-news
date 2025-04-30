514224e05037736ebfdfe532708630664009f5e9
Кухарук Александр <okukharuk@expertus.com.ua>
2025-04-30 21:36:34 +0300

    added login request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,24 +6,21 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-            'device_name' => 'required',
-        ]);
+        $data = $request->validated();
 
-        $user = User::where('email', $request->email)->first();
+        $user = User::where('email', $data->email)->first();
 
-        if (! $user || ! Hash::check($request->password, $user->password)) {
+        if (!$user || ! Hash::check($data->password, $user->password)) {
             return response()->json(['message' => 'The provided credentials are incorrect.'], 401);
         }
 
-        return ['token' => $user->createToken($request->device_name)->plainTextToken];
+        return ['token' => $user->createToken($data->device_name)->plainTextToken];
     }
 
     public function logout(Request $request)
